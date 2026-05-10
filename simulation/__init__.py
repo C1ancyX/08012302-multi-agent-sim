e4043f0d1092de914c25f86675f6b6ebb3c6e04d
C1ancyX <Keransh@outlook.com>
2026-05-11 04:45:51 +0800

    Delete simulation directory

--- simulation/__init__.py
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-from simulation.env import MultiAgentEnv
-from simulation.renderer import PygameRenderer
-from simulation.manual_controller import ManualController
-
-__all__ = ['MultiAgentEnv', 'PygameRenderer', 'ManualController']
